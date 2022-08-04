b0f075d7967fd6328ef3fae24479c83626aa3c87
Jason Haugland <jasonhaugland@gmail.com>
2022-08-04 17:45:11 -0500

    missed files

---
+++ java-jedis/src/main/java/com/redis/searchstock/RedisearchStocksRestApplication.java
@@ -0,0 +1,13 @@
+package com.redis.searchstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisearchStocksRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisearchStocksRestApplication.class, args);
+	}
+
+}
